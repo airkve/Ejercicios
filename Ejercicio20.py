11ed436e93b8776d3e66fa31044654e624a1f177
Richard Jimenez <ricjim@gmail.com>
2019-04-03 16:29:54 -0300

    Add files via upload

---
+++ Ejercicio20.py
@@ -0,0 +1,13 @@
+# Crear un programa que pida un numero N e introducir sueldos por ese cantidad de N
+# Mostrar el sueldo maximo
+
+n = int(input('Introduce un numero: '))
+
+for i in range(n):
+    sueldo = int(input('Ingrese el sueldo: '))
+    var + i = sueldo
+
+    if sueldo > var + i:
+        result = sueldo
+    else:
+        result = var + i
