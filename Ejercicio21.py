5caf2ccdbcdacbca695b7fd761bce39b829242b4
Richard Jimenez <ricjim@gmail.com>
2019-04-03 16:47:21 -0300

    Add files via upload

--- Ejercicio21.py
+++ Ejercicio21.py
@@ -12,9 +12,15 @@ for i in range(10):
 
     if n < 0:
         neg += 1
-    elif n % 3 = 0:
+    elif n % 3 == 0:
         mult3 += 1
     elif n < 6:
         less6 += 1
+if neg > 0:
+    print('Se encontraron numeros negativos')
+elif mult3 > 0:
+    print('Se encontraron numeros multiplos de 3')
+if less6 > 0:
+    print('Se encontraron numeros menores a 6')
 
-print('Se ingresaron {} numeros positivos, {} numeros multiplos de 3 y {} numeros menor que 6.'.format(neg, mult3, less6))
+print('Se ingresaron {} numeros negativos, {} numeros multiplos de 3 y {} numeros menor que 6.'.format(neg, mult3, less6))
