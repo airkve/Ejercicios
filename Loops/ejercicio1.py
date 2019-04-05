37d429a29cb389b7edfb8a4a16bf2031e2c01b7b
Richard Jimenez <ricjim@gmail.com>
2019-04-05 15:15:01 -0300

    Delete ejercicio1.py

--- Loops/ejercicio1.py
+++
@@ -1,9 +0,0 @@
-# Pedir un numero y mostrar su cuadrado, repetir este proceso hasta
-# que se introduzca un mumero negativo
-
-numero = 0
-
-while numero >= 0:
-    numero = int(input('Escribe un numero positivo: '))
-    print('{}'.format(numero**2))
-print('Programa terminado.')
