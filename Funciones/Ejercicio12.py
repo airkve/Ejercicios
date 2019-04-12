6bb3cedda823cefa0285d46c49d6aa9597725f2d
Richard Jimenez <ricjim@gmail.com>
2019-04-12 15:02:59 -0300

    Add files via upload

---
+++ Funciones/Ejercicio12.py
@@ -0,0 +1,18 @@
+# Crear una funcion que decida si dos numeros enteros positivos, son amigos
+# 2 numeros son amigos si, la suma de sus divisores son iguales al otro numero.
+
+def numAmigos(num1, num2):
+    totalNum1 = 0
+    totalNum2 = 0
+    for i in range(1, num1):
+        if num1 % i == 0:
+            totalNum1 += i
+    for j in range(1, num2):
+        if num2 % j == 0:
+            totalNum2 += j
+    if totalNum1 == num2 and totalNum2 == num1:
+        print('Los numeros {} y {}, son amigos'.format(num1, num2))
+    else:
+        print('Los numeros {} y {}, no son amigos'.format(num1, num2))
+
+numAmigos(284, 220)
