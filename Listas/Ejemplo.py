352bdfe1038d3d806ae84d395366c1a24e9ecfa2
Richard Jimenez <ricjim@gmail.com>
2019-04-17 15:56:56 -0300

    Add files via upload

---
+++ Listas/Ejemplo.py
@@ -0,0 +1,13 @@
+# Ejemplos de listas
+
+[1, 2, 3]
+['gato', 'perro', 'rata', 'elefante']
+spam = ['gato', 'perro', 'rata', 'elefante'] # Lista de cadenas
+print(spam)
+print(spam[3])
+print('hola' + spam[0])
+print('El ' + spam[0] + ' se comio la ' + spam[0])
+
+spam1 = [['gato', 'perro'], [10, 20, 30, 40, 50]] # Nested list
+print(spam[-1]) # Indice negativo
+rango = spam[1:3] # Slices
