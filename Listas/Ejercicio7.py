41acf69528b532524370c10cb88618500769dbef
Richard Jimenez <ricjim@gmail.com>
2019-04-26 12:43:30 -0300

    Update Ejercicio7.py

--- Listas/Ejercicio7.py
+++ Listas/Ejercicio7.py
@@ -1,26 +1,26 @@
 # Crear una funcion que indique si los numeros ingresados manalmente, estan
 # en orden creciente, decreciente o desordenados
 
-def listaAsc(lista):
-    result = False
-    for i in range(len(lista)):
-        if lista[i] < lista[i+1]
-            result = True
-    return result
+def getOrder(lst):
+    result = []
 
-def listaPrint():
-    nums = []
+    for i in range(len(lst) - 1):
+        if lst[i] < lst[i + 1]:
+            result.append(True)
+        elif lst[i] > lst[i + 1]:
+            result.append(False)
 
+    if False not in result:
+        print('La lista está en orden ascendente')
+    elif True not in result:
+        print('La lista está en orden decendente')
+    else:
+        print('La lista está desordenada')
+
+def newList():
     for i in range(10):
         question = int(input('Introduce un numero: '))
         nums.append(question)
 
-    for j in range(len(nums)):
-        if nums[j] < nums[j+1]:
-            print('Es ascendente')
-        elif nums[j] > nums[j+2]:
-            print('Es decendente')
-        else:
-            print('Esta desordenada')
-            
-listaPrint()
+newList()
+getOrder(nums)
